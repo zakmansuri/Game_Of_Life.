1ef7bce4dee78187b3dfb3bf272ee00e6ecea994
georgecorfield <gacorfield50@gmail.com>
2023-10-27 16:48:51 +0100

    connected IO and Distributor via channels and populated world

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,6 +1,9 @@
 package gol
 
-import "uk.ac.bris.cs/gameoflife/util"
+import (
+	"fmt"
+	"uk.ac.bris.cs/gameoflife/util"
+)
 
 type distributorChannels struct {
 	events     chan<- Event
@@ -14,12 +17,18 @@ type distributorChannels struct {
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 	// TODO: Create a 2D slice to store the world.
+	c.ioCommand <- ioInput
+	c.ioFilename <- fmt.Sprintf("%d%s%d", p.ImageHeight, "x", p.ImageWidth)
 	IMHT := p.ImageHeight
 	IMWD := p.ImageWidth
 	world := make([][]byte, IMHT)
-	for i := range world {
-		world[i] = make([]byte, IMWD)
+	for y := range world {
+		world[y] = make([]byte, IMWD)
+		for x := range world[y] {
+			world[y][x] = <-c.ioInput
+		}
 	}
+
 	turn := 0
 	// TODO: Execute all turns of the Game of Life.
 	for turn <= p.Turns {
