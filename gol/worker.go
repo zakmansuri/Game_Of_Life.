6001e53f75ed7e2a4c6813bd8449aedb46965989
Zak Mansuri <77395132+zakmansuri@users.noreply.github.com>
2023-10-31 17:00:05 +0000

    Create worker.go

---
+++ gol/worker.go
@@ -0,0 +1,11 @@
+package gol
+
+func worker(p Params, bh int, h int, world [][]byte, out chan<- [][]byte, t int, c distributorChannels) {
+	temp := make([][]byte, p.ImageWidth)
+	for i := 0; i < p.ImageWidth; i++ {
+		temp[i] = make([]byte, p.ImageWidth)
+	}
+	newSlice := updateW(p, world, temp, bh, h, t, c)
+
+	out <- newSlice
+}
