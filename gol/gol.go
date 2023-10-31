21461517323dd0436ebb824f60011b026999f4cd
Zak Mansuri <77395132+zakmansuri@users.noreply.github.com>
2023-10-31 17:00:54 +0000

    Update gol.go

--- gol/gol.go
+++ gol/gol.go
@@ -35,6 +35,8 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 		ioFilename: ioFilename,
 		ioOutput:   ioOutput,
 		ioInput:    ioInput,
+		keyPresses: keyPresses,
 	}
 	distributor(p, distributorChannels)
 }
+
