f272bedf290230a24a40ef758eafa52ce99739af
Zak Mansuri <77395132+zakmansuri@users.noreply.github.com>
2023-11-20 17:47:08 +0000

    Update worker.go

--- Parallel/gol/worker.go
+++ Parallel/gol/worker.go
@@ -1,11 +1,14 @@
 package gol
 
+// worker function is designed to update a slice of the Game of Life world
 func worker(p Params, bh int, h int, world [][]byte, out chan<- [][]byte, t int, c distributorChannels) {
+	// Initialize a temporary slice to store the updated state of the worlds slice.
 	temp := make([][]byte, p.ImageWidth)
-	for i := 0; i < p.ImageWidth; i++ {
+	for i := range temp {
 		temp[i] = make([]byte, p.ImageWidth)
 	}
-	newSlice := updateNextState(p, world, temp, bh, h, t, c)
 
-	out <- newSlice
+	out <- updateNextState(p, world, temp, bh, h, t, c)
+
 }
+
