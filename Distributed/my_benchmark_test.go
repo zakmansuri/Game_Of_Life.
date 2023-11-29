9928921067467b9aa01414146ccea2431ed99b62
georgecorfield <gacorfield50@gmail.com>
2023-11-29 19:12:25 +0000

    benchmarking

--- Distributed/my_benchmark_test.go
+++ Distributed/my_benchmark_test.go
@@ -13,22 +13,21 @@ import (
 const benchLength = 1000
 
 func BenchmarkStudentVersion(b *testing.B) {
-	for threads := 1; threads <= 16; threads++ {
-		os.Stdout = nil // Disable all program output apart from benchmark results
-		p := gol.Params{
-			Turns:       benchLength,
-			Threads:     threads,
-			ImageWidth:  512,
-			ImageHeight: 512,
-		}
-		name := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
-		b.Run(name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				events := make(chan gol.Event)
-				go gol.Run(p, events, nil)
-				for range events {
-				}
-			}
-		})
+	//for threads := 1; threads <= 16; threads++ {
+	os.Stdout = nil // Disable all program output apart from benchmark results
+	p := gol.Params{
+		Turns:       benchLength,
+		Threads:     1,
+		ImageWidth:  512,
+		ImageHeight: 512,
 	}
+	name := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
+	b.Run(name, func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			events := make(chan gol.Event)
+			go gol.Run(p, events, nil)
+			for range events {
+			}
+		}
+	})
 }
